611bf0cb0e7769a23853dcb62b1e92f45c6bbc2d
dhthanh <dhthanhqa@gmail.com>
2020-07-27 11:09:35 +0700

    Update code 2707

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -16,15 +16,72 @@ def read_products(keyword=None, from_price=None, to_price=None):
     return products
 
 
+def read_product_by_id(product_id):
+    products = read_products()
+    for p in products:
+        if p["id"] == product_id:
+            return p
+
+    return None
+
+
+def update_product(product_id, name, description, price, image, category):
+    products = read_products()
+    for p in products:
+        if p["id"] == int(product_id):
+            p["name"] = name
+            p["description"] = description
+            p["price"] = float(price)
+            p["image"] = image
+            p["category_id"] = int(category)
+
+            break
+
+    return update_product_json(products)
+
+
+def delete_product(product_id):
+    products = read_products()
+    for idx, p in enumerate(products):
+        if p["id"] == int(product_id):
+            del products[idx]
+            break
+
+    return update_product_json(products)
+
+
+def add_product(name, description, price, image, category):
+    products = read_products()
+    products.append({
+        "id": len(products) + 1,
+        "name": name,
+        "description": description,
+        "price": float(price),
+        "image": image,
+        "category_id": int(category)
+    })
+
+    return update_product_json(products)
+
+
+def update_product_json(products):
+    try:
+        with open(os.path.join(app.root_path, "data/products.json"), "w", encoding="utf-8") as f:
+            json.dump(products, f, ensure_ascii=False, indent=4)
+
+        return True
+    except Exception as ex:
+        print(ex)
+        return False
+
+
+def read_categories():
+    with open(os.path.join(app.root_path, "data/categories.json"), encoding="utf-8") as f:
+        return json.load(f)
+
+
 def read_products_by_cate_id(cate_id):
     return [product for product in read_products() if product["category_id"] == cate_id]
-    # products = read_products()
-    # result = []
-    # for product in products:
-    #     if product.category_id == cate_id:
-    #         result.append(product)
-    #
-    # return result
 
 
 if __name__ == "__main__":
--- saleapp/main.py
+++ saleapp/main.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, jsonify
 from saleapp import app
 from saleapp import dao
 
@@ -22,6 +22,44 @@ def product_list():
 @app.route("/products/<int:category_id>")
 def product_list_by_cate(category_id):
     return render_template("product-list.html", products=dao.read_products_by_cate_id(cate_id=category_id))
+
+
+@app.route("/api/products/<int:product_id>", methods=["delete"])
+def delete_product(product_id):
+    if dao.delete_product(product_id=product_id):
+        return jsonify({"status": 200, "product_id": product_id})
+
+    return jsonify({"status": 500, "error_message": "Something Wrong!!!"})
+
+
+@app.route("/products/add", methods=["get", "post"])
+def add_product():
+    """
+    add: /products/add
+    update: /products/add?product_id
+    :return: template
+    """
+    err_msg = None
+    if request.method.lower() == "post":
+        if request.args.get("product_id"): # UPDATE
+            d = dict(request.form.copy())
+            d["product_id"] = request.args["product_id"]
+            if dao.update_product(**d):
+                return redirect(url_for('product_list', product_id=d["product_id"]))
+            else:
+                err_msg = "Something wrong, go back later!!!"
+        else: # ADD
+            if dao.add_product(**dict(request.form)):
+                return redirect(url_for('product_list'))
+            else:
+                err_msg = "Something wrong, go back later!!!"
+
+    categories = dao.read_categories()
+    product = None
+    if request.args.get("product_id"):
+        product = dao.read_product_by_id(product_id=int(request.args["product_id"]))
+
+    return render_template("product-add.html", categories=categories, product=product, err_msg=err_msg)
 
 
 if __name__ == "__main__":
